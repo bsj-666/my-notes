60a1883f1b804970ca56be6eb00349ce4ea0eaeb
sj b <1420306308@qq.com>
2026-03-26 15:58:02 +0800

    docs: update notes

--- docs/.vitepress/sidebar.mjs
+++ docs/.vitepress/sidebar.mjs
@@ -3,7 +3,7 @@ export const sidebar = [
     text: '笔记',
     items: [
     {
-        "text": "时区已经是对的（Asia/Shanghai），我们只需要把时间改成正确的北京时间",
+        "text": "运维",
         "link": "/notes/yunwei"
     }
 ]
