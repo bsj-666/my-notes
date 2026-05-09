2c9a1b2973da41b1dffd52b183b1c1c05c722098
sj b <1420306308@qq.com>
2026-05-09 14:35:11 +0800

    docs: update notes

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,28 +1,27 @@
 import { defineConfig } from 'vitepress'
-import { sidebar } from './sidebar.mjs'
 import { withMermaid } from 'vitepress-plugin-mermaid'
+import { sidebar } from './sidebar.mjs'
 
-export default defineConfig({
-  title: 'SJ 的笔记站',
-  description: '个人笔记网站',
-  lang: 'zh-CN',
-  base: '/my-notes/',
-	
-  themeConfig: {
-    nav: [
-      { text: '首页', link: '/' }
-    ],
-	
-	sidebar,
-	
-	aside: 'right',
-	outline: {
-    label: '目录',
-    level: [2, 3]
-	},
+export default withMermaid(
+  defineConfig({
+    title: 'SJ 的笔记站',
+    description: '个人笔记网站',
+    lang: 'zh-CN',
+    base: '/my-notes/',
 
-    socialLinks: [
-      { icon: 'github', link: 'https://bsj-666.github.io/my-notes' }
-    ]
-  }
-})
+    themeConfig: {
+      nav: [
+        { text: '首页', link: '/' }
+      ],
+      sidebar,
+      aside: 'right',
+      outline: {
+        label: '目录',
+        level: [2, 3]
+      },
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/bsj-666/my-notes' }
+      ]
+    }
+  })
+)
